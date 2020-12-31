ab0b19410deb0c7c2ce103d6cffd15c2b786b497
Michael Parkadze <52443367+parkadze-dev@users.noreply.github.com>
2020-12-31 21:55:37 +0200

    [Update] Timer Functionality
    base timer functionality is done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { useInterval } from "./hooks/useInterval";
 
 function App() {
+  const [time, setTime] = useState(1500000);
+  const [started, setStarted] = useState(false);
+
+  useInterval(() => setTime(time - 1000), started ? 1000 : null);
+
+  const handleButtonClick = () => {
+    setStarted(!started);
+  };
+
+  const handleResetTimer = () => {
+    setStarted(false);
+    setTime(1500000);
+  };
+
+  const Timer = () => {
+    let minutes = Math.floor(time / 60000);
+    let seconds = ((time % 60000) / 1000).toFixed(0);
+    return (
+      <>
+        {minutes < 10 ? <span>0{minutes}</span> : <span>{minutes}</span>}:
+        {seconds < 10 ? <span>0{seconds}</span> : <span>{seconds}</span>}
+      </>
+    );
+  };
+
+  const handleIncrement = () => {
+    setTime(time + 60000);
+  };
+
+  const handleDecrement = () => {
+    setTime(time - 60000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pomodoro">
+      <div className="pomodoro__timer">
+        <Timer />
+      </div>
+      <input
+        type="number"
+        value={Math.floor(time / 60000)}
+        disabled
+        // onChange={(e) => setTime(e.target.value * 60000)}
+      />
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleButtonClick}>{started ? "Stop" : "Start"}</button>
+      <button onClick={handleResetTimer}>Reset</button>
     </div>
   );
 }
